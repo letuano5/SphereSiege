08bc369359fd859873b67b7be02f78804c6225f2
Pham Thong <phucon547@gmail.com>
2024-03-24 08:11:02 +0700

    make itemProgressBars work

--- Items.h
+++ Items.h
@@ -5,6 +5,7 @@
 #include "ProgressBar.h"
 // seconds
 const double DIF_SPAWN_TIME = 1;
+const int activeTime = 5;
 const string itemTypes[] = {"FAST_SHOT", "HP_PACK", "SLOWDOWN_ENEMIES", "TRIPPLE_SHOT", "PIERCE_SHOT"};
 
 class Items {
@@ -14,10 +15,12 @@ class Items {
     void spawnItem(Hero& hero, const Camera& camera, MultiEnemy& enemies);
     clock_t lastTimeSpawned = clock();
     bool checkTime();
-    pair<string, SDL_Color> itemProgressTypes[4] = {{"FAST SHOT", {138, 201, 38, 255}}, {"TRIPPLE SHOT", {0, 180, 216, 255}}, {"PIERCE SHOT", {243, 144, 44, 255}}, {"SLOWDOWN ENEMIES", {128, 128, 128, 255}}};
-    ProgressBar* itemsProgress[4] = {NULL, NULL, NULL, NULL};
+    pair<string, SDL_Color> itemProgressTypes[4] = {{"FAST_SHOT", {138, 201, 38, 255}}, {"TRIPPLE_SHOT", {0, 180, 216, 255}}, {"PIERCE_SHOT", {243, 144, 44, 255}}, {"SLOWDOWN_ENEMIES", {128, 128, 128, 255}}};
+    vector<pair<string, ProgressBar*>> itemsProgress;
 
    private:
     vector<Item*> items;
     map<string, Uint32> activeItems;
+    SDL_Color textColor = {100, 100, 100, 255};
+    SDL_Color textActiveColor = {188, 188, 188, 255};
 };
