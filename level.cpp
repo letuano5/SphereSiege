4ae44401040395a97dc168ac73b8ce69c5c971dc
letuano5 <jssjsonsn@gmail.com>
2024-03-28 20:28:36 +0700

    lvl up sound

--- level.cpp
+++ level.cpp
@@ -2,14 +2,14 @@
 #include "Level.h"
 
 Level::Level() {
-//    levelup_sound = Mix_LoadWAV("res/audio/levelUp.wav");
-//    if (levelup_sound == NULL) {
-//        cerr << "Failed to load levelup sound.\n";
-//    }
+    levelup_sound = Mix_LoadWAV("res/audio/levelUp.wav");
+    if (levelup_sound == NULL) {
+        cerr << "Failed to load levelup sound.\n";
+    }
 }
 
 Level::~Level() {
-//    Mix_FreeChunk(levelup_sound);
+    Mix_FreeChunk(levelup_sound);
 }
 
 void Level::update(int curScore) {
@@ -21,9 +21,11 @@ void Level::update(int curScore) {
     if (this->curScore != curScore) {
         this->curScore = curScore;
         if (curScore >= curScoreNeeded && level + 1 < 5) {
-//            if (!isMuted && Mix_PlayChannel(-1, levelup_sound, 0) == -1) {
-//                cerr << "Failed to play levelup sound. Error: " << Mix_GetError() << "\n";
-//            }
+            int play = Mix_PlayChannel(-1, levelup_sound, 0);
+            cerr << "Level up! " << play << endl;
+            if (play == -1) {
+                cerr << "Failed to play levelup sound. Error: " << Mix_GetError() << "\n";
+            }
             level++;
         }
     }
@@ -32,7 +34,7 @@ void Level::update(int curScore) {
 double Level::getLevelProgress() {
     int last = 0;
     for (int i = 0; i < level; i++) last += levelData[i].second;
-//    cerr << "? " << last << " " << level << endl;
+//    cerr << "? " << curScore << " " << last << " " << level << endl;
     return (double)(curScore - last) / levelData[level].second;
 }
 
